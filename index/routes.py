10f0f30e9a079e64771b59c25a20c77ac4a385a1
Guilherme O. Souza <ShadowDragonRub@gmail.com>
2022-11-13 20:26:35 -0300

    p12

--- index/routes.py
+++ index/routes.py
@@ -9,6 +9,6 @@ index_blueprint = Blueprint("index_blueprint", __name__)
 @index_blueprint.route("/index", methods=["GET"])
 def index():
 
-    current_timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    current_timestamp = datetime.now().strftime("%H:%M:%S")
 
     return render_template("index.html", now=current_timestamp)
